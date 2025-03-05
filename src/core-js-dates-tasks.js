62a1c33d761ffd192ab5f47dbace9173524a2db5
Serj Lyakhov <serjml@list.ru>
2025-03-05 19:53:46 +0300

    feat: complete getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,19 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wensday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const currentDayNumber = new Date(date).getDay();
+  return weekDays[currentDayNumber];
 }
 
 /**
