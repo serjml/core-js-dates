f9f0c66170744acce772b8821d7201e02139a7be
Serj Lyakhov <serjml@list.ru>
2025-03-05 21:13:22 +0300

    feat: complete getCountDaysInMonth & isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -112,8 +112,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const timestampDayDifference = new Date(dateEnd) - new Date(dateStart);
+  const dayDifference = timestampDayDifference / 1000 / 60 / 60 / 24;
+  return dayDifference + 1;
 }
 
 /**
@@ -133,8 +135,9 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const { start, end } = period;
+  return new Date(end) >= new Date(date) && new Date(start) <= new Date(date);
 }
 
 /**
