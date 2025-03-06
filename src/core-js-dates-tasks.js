1092f1da685a67af3d884e20383c60afe9f621c5
Serj Lyakhov <serjml@list.ru>
2025-03-06 21:06:58 +0300

    feat: complete getWorkSchedule

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -264,8 +264,32 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const { start, end } = period;
+  const [startDay, startMonth, startYear] = start.split('-');
+  const [endDay, endMonth, endYead] = end.split('-');
+
+  const startDate = new Date(startYear, startMonth - 1, startDay);
+  const endDate = new Date(endYead, endMonth - 1, endDay);
+  const schedule = [];
+  const currentDate = startDate;
+
+  while (currentDate <= endDate) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+      const year = currentDate.getFullYear();
+
+      schedule.push(`${day}-${month}-${year}`);
+      currentDate.setDate(currentDate.getDate() + 1);
+      if (currentDate >= endDate) {
+        break;
+      }
+    }
+
+    currentDate.setDate(currentDate.getDate() + countOffDays);
+  }
+  return schedule;
 }
 
 /**
