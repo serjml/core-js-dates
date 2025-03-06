e3a65494cfb9635d60cc35e982dec36b08cd970b
Serj Lyakhov <serjml@list.ru>
2025-03-06 20:20:54 +0300

    feat: complete getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -220,8 +220,14 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentDate = new Date(date);
+
+  while (!(currentDate.getDay() === 5 && currentDate.getDate() === 13)) {
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return new Date(currentDate);
 }
 
 /**
