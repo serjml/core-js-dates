7cfc02550cec8e224fec5953649f09243138ebf0
Serj Lyakhov <serjml@list.ru>
2025-03-06 19:43:08 +0300

    feat: complete getWeekNumberByDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -202,8 +202,11 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const year = date.getFullYear();
+  const yearStart = new Date(year, 0);
+  const dayOfYear = (date - yearStart) / (1000 * 60 * 60 * 24) + 1;
+  return Math.ceil(dayOfYear / 7);
 }
 
 /**
