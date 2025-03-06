aeb7395b55ae867b80672b31d3ce5738c91a73ab
Serj Lyakhov <serjml@list.ru>
2025-03-06 20:25:14 +0300

    feat: complete getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -241,8 +241,9 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const currentMonth = date.getUTCMonth();
+  return Math.ceil((currentMonth + 1) / 3);
 }
 
 /**
